a0a51f038822d27534f142b264cb627f05ef423d
Sam Ames <samames9@icloud.com>
2024-03-08 17:32:57 +0000

    login button working

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import "@/assets/styles/globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import AuthProvider from "@/components/AuthProvider";
 
 export const metadata = {
   title: "property pulse",
@@ -8,13 +9,15 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        <main>{children}</main>;
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body>
+          <Navbar />
+          <main>{children}</main>;
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 
